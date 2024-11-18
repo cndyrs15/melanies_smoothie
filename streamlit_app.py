804493dc6e468dc191272c0723993448caee93c0
cndyrs15 <cindy.rose.santos@accenture.com>
2024-11-18 17:25:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be", name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+#cnx = st.connection("snowflake")
+#session = cnx.session()
 
 
 session = get_active_session()
